67770267959e78f183f6d55b6c678d781919fe56
Mayank Ashok Gongal <111949604+mayankgongal5@users.noreply.github.com>
2024-12-15 21:48:01 +0530

    hee12

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const aiRoutes = require('./routes/aiRoutes');
-const psRoutes = require('./routes/psRoutes');
-const daaRoutes = require('./routes/daaRoutes');
+const express = require("express");
+const aiRoutes = require("./routes/aiRoutes");
+const psRoutes = require("./routes/psRoutes");
+const daaRoutes = require("./routes/daaRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Register route modules
-app.use('/ai', aiRoutes);
-app.use('/ps', psRoutes);
-app.use('/daa', daaRoutes);
+app.use("/ai", aiRoutes);
+app.use("/ps", psRoutes);
+app.use("/daa", daaRoutes);
 
 // Default route
-app.get('/', (req, res) => {
-  res.send('Welcome to the API server');
+app.get("/", (req, res) => {
+  res.send("NOT FOUND");
 });
 
 // Handle 404
 app.use((req, res) => {
-  res.status(404).send('Route not found');
+  res.status(404).send("Route not found");
 });
 
 app.listen(port, () => {
